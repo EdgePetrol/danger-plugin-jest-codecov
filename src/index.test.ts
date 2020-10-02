ffb975e17c8bc6103688ab89d8ec6c3f822a8842
Guilherme Pereira <guiferrpereira@gmail.com>
2020-10-02 01:23:37 +0100

    [EDG-3124] Add tests

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,23 +1,68 @@
 import { jestCodecov } from "./index"
+import fetch from "node-fetch"
 
 declare const global: any
 
+let mockFetchPromise
+
+jest.mock('node-fetch', () => ({
+  default: jest.fn().mockImplementation((url: string) => mockFetchPromise(url)),
+}))
+
 describe("#jestCodecov()", () => {
   beforeEach(() => {
-    global.warn = jest.fn()
-    global.message = jest.fn()
-    global.fail = jest.fn()
     global.markdown = jest.fn()
   })
 
   afterEach(() => {
-    global.warn = undefined
-    global.message = undefined
-    global.fail = undefined
     global.markdown = undefined
   })
 
-  it("returns undefined without no circle ci envs", async () => {
-    expect(await jestCodecov("http://localhost/index.html", "http://localhost/prev_index.html")).toBe(undefined)
+  describe("valid reports", () => {
+    it("returns an invalid report", async () => {
+      expect(await jestCodecov("http://localhost/index.html", "http://localhost/old-index.html")).toBe(undefined)
+    })
+  })
+
+  describe("valid reports", () => {
+    beforeEach(() => {
+      const newHtml = require('./test-files/index.html')
+      const oldHtml = require('./test-files/old-index.html')
+
+      const mockSuccessResponse = (url: string) => {
+        if (url.match(/old-index/)) {
+          return newHtml
+        }
+        return oldHtml
+      }
+      const mockHtmlPromise = (url: string) => Promise.resolve(mockSuccessResponse(url))
+
+      mockFetchPromise = (url: string) => Promise.resolve({
+        text: () => mockHtmlPromise(url),
+      })
+    })
+
+    describe("with difference", () => {
+      it("returns the correct report with positive values", async () => {
+        await jestCodecov("http://localhost/index.html", "http://localhost/old-index.html")
+        
+        expect(global.markdown).toHaveBeenCalled()
+        expect(global.markdown).toHaveBeenCalledTimes(1) 
+      })
+
+      it("returns the correct report with negative values", async () => {
+        await jestCodecov("http://localhost/old-index.html", "http://localhost/index.html")
+        
+        expect(global.markdown).toHaveBeenCalled()
+        expect(global.markdown).toHaveBeenCalledTimes(1)
+      })
+
+      it("returns the correct report with the same values", async () => {
+        await jestCodecov("http://localhost/index.html", "http://localhost/index.html")
+        
+        expect(global.markdown).toHaveBeenCalled()
+        expect(global.markdown).toHaveBeenCalledTimes(1)
+      })
+    })
   })
 })
